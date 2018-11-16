b9fb55bf5088c4c1af762224f933ae08de7add14
Brandon <b.charest019@gmail.com>
2018-11-16 08:41:24 -0500

    MaximumBinaryTree

--- MaximumBinaryTree/main.cpp
+++ MaximumBinaryTree/main.cpp
@@ -1,12 +1,117 @@
-#include "main.h"
+/*654. Maximum Binary Tree
 
+Given an integer array with no duplicates. A maximum tree building on this array is defined as follow:
 
+The root is the maximum number in the array.
+The left subtree is the maximum tree constructed from left part subarray divided by the maximum number.
+The right subtree is the maximum tree constructed from right part subarray divided by the maximum number.
+Construct the maximum tree by the given array and output the root node of this tree.
 
-main::main()
+Input: [3,2,1,6,0,5]
+Output: return the tree root node representing the following tree:
+
+	6
+  /   \
+ 3     5
+  \    /
+   2  0
+    \
+     1
+*/
+
+//Definition for a binary tree node.
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	
+};
+
+void printTree(TreeNode const *node)
 {
+	if(node != nullptr)
+	{
+		std::cout << node->val << " ";
+		printTree(node->left);
+		printTree(node->right);
+	}
+}
+
+void add_node(TreeNode *&node, int val)
+{
+	if(node == nullptr)
+	{
+		node = new TreeNode(val);
+	}
+	else if(val < node->val)
+	{
+		add_node(node->left, val);
+	}
+	else
+	{
+		add_node(node->right, val);
+	}
 }
 
 
-main::~main()
+TreeNode* constructMaximumBinaryTree(std::vector<int>& nums) 
 {
+	if(nums.empty())
+	{
+		return nullptr;
+	}
+
+	auto const maxNum =std::distance(nums.begin(),std::max_element(nums.begin(), nums.end()));
+
+	auto *maxTree = new TreeNode(nums[maxNum]);
+
+	std::vector<int> leftSide = {};
+	std::vector<int> rightSide = {};
+
+	for(auto i = 0; i < nums.size(); i++)
+	{
+		if(i < maxNum)
+		{
+			leftSide.push_back(nums[i]);
+		}
+		else if(i > maxNum)
+		{
+			rightSide.push_back(nums[i]);
+		}
+	}
+
+	std::sort(leftSide.begin(), leftSide.end(), std::greater<>());
+	std::sort(rightSide.begin(), rightSide.end(), std::greater<>());
+
+
+	for (auto value : leftSide)
+	{
+		add_node(maxTree->left, value);
+	}
+
+	for (auto value : rightSide)
+	{
+		add_node(maxTree->right, value);
+	}
+	return maxTree;
+}
+
+
+int main()
+{
+	std::vector<int> numbers = { 3,2,1,6,0,5 };
+
+	
+
+	TreeNode *tree = constructMaximumBinaryTree(numbers);
+
+	printTree(tree);
+
+	return 0;
 }
