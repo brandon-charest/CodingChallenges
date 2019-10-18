a4d6f6164885c0323d647dee5dfc08e9c179f077
Brandon Charest <b.charest019@gmail.com>
2019-10-18 16:01:35 -0400

    added description

--- Python/Maximum_Level_Sum_Tree.py
+++ Python/Maximum_Level_Sum_Tree.py
@@ -1,3 +1,17 @@
+""" 1161. Maximum Level Sum of a Binary Tree
+Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
+
+Return the smallest level X such that the sum of all the values of nodes at level X is maximal.
+
+ 
+Input: [1,7,0,7,-8,null,null]
+Output: 2
+Explanation: 
+Level 1 sum = 1.
+Level 2 sum = 7 + 0 = 7.
+Level 3 sum = 7 + -8 = -1.
+So we return the level with the maximum sum which is level 2.
+"""
 from Coding.Binary_Tree import Tree
 from collections import deque
 
