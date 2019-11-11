22215f999c04cabfdb1c90813a7b4ee283a526de
Brandon Charest <b.charest019@gmail.com>
2019-11-11 15:45:23 -0500

    Create Assign_Cookies.py

---
+++ Python/Assign_Cookies.py
@@ -0,0 +1,63 @@
+""" 455. Assign Cookies
+Assume you are an awesome parent and want to give your children some cookies. But, you should give each child at most
+one cookie. Each child i has a greed factor gi, which is the minimum size of a cookie that the child will be
+content with; and each cookie j has a size sj. If sj >= gi, we can assign the cookie j to the child i,
+and the child i will be content. Your goal is to maximize the number of your content children
+and output the maximum number.
+
+Note:
+You may assume the greed factor is always positive.
+You cannot assign more than one cookie to one child.
+
+Example 1:
+Input: [1,2,3], [1,1]
+
+Output: 1
+Explanation: You have 3 children and 2 cookies. The greed factors of 3 children are 1, 2, 3.
+And even though you have 2 cookies, since their size is both 1, you could only make the child whose greed
+factor is 1 content.
+You need to output 1.
+
+Example 2:
+Input: [1,2], [1,2,3]
+
+Output: 2
+Explanation: You have 2 children and 3 cookies. The greed factors of 2 children are 1, 2.
+You have 3 cookies and their sizes are big enough to gratify all of the children,
+You need to output 2.
+"""
+
+from typing import List
+import unittest
+
+
+def assign_cookie(greed: List[int], size: List[int]) -> int:
+    result = i = 0
+    greed.sort()
+    size.sort()
+
+    for g in greed:
+        while i < len(size) and g > size[i]:
+            i += 1
+        if i == len(size):
+            break
+        result += 1
+        i += 1
+    return result
+
+
+class Test_Assign_Cookie(unittest.TestCase):
+    def test_returns1(self):
+        test_greed = [1, 2, 3]
+        test_size = [[1, 1], [3]]
+        for s in test_size:
+            assert assign_cookie(test_greed, s) == 1
+
+    def test_returns2(self):
+        test_greed = [10, 9, 8, 7]
+        test_size = [5, 6, 7, 8]
+        assert assign_cookie(test_greed, test_size) == 2
+
+
+if __name__ == '__main__':
+    unittest.main()
