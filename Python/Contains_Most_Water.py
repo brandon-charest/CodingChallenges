b60339eabf96eac15e875424fcd676c0e41ce1cc
Brandon Charest <b.charest019@gmail.com>
2019-11-21 09:07:20 -0500

    Update Contains_Most_Water.py

--- Python/Contains_Most_Water.py
+++ Python/Contains_Most_Water.py
@@ -1,5 +1,7 @@
 """ 11. Container With Most Water
-Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). n vertical
+lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together
+with x-axis forms a container, such that the container contains the most water.
 
 Note: You may not slant the container and n is at least 2.
 Example:
@@ -24,6 +26,7 @@ def max_area(height: List[int]) -> int:
             left += 1
     return area
 
+
 class Test_Max_Area(unittest.TestCase):
 
     def test_returns_49(self):
@@ -37,6 +40,7 @@ class Test_Max_Area(unittest.TestCase):
 
     def test_returns_24(self):
         assert max_area([1,3,2,5,25,24,5]) == 24
+
 
 if __name__ == '__main__':
     unittest.main()
